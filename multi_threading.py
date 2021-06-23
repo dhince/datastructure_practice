52934ccdb49fdb71f2c2b505aa0d9ddef87d805a
dhince <copymasternp@gmail.com>
2021-06-23 16:07:19 +0530

    some comments are added for understanding purpose.

--- multi_threading.py
+++ multi_threading.py
@@ -1,7 +1,7 @@
 from time import sleep
 from threading import Thread
 class Hello(Thread):
-    def run(self):
+    def run(self):      #You always need to use name of function as run cz it is built-in
         for _ in range(5):
 
             print('Hello')
@@ -16,11 +16,11 @@ class Hi(Thread):
 
 t1 = Hello()
 t2 = Hi()
-t1.start()
+t1.start()    #instead of t1.run you have to use t1.start to create a different thread.
 sleep(0.2)
 t2.start()
 
-t1.join()
-t2.join()
+t1.join()  #this will make sure main thread to wait for thread t1 to get executed first
+t2.join()  #this will make sure main thread to wait for thread t2 to get executed first
 
 print('Bye')
